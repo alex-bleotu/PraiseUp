4c2104332403ee3a21c336cf9db78d2bc2dd2493
AlexDavid26 <82370626+alex-bleotu@users.noreply.github.com>
2024-11-11 18:39:23 +0200

    Upload code working

--- extra/upload/upload.js
+++ extra/upload/upload.js
@@ -1,53 +1,79 @@
-const { Storage } = require("@google-cloud/storage");
+const { initializeApp } = require("firebase/app");
+const { getAuth, signInWithCustomToken } = require("firebase/auth");
+const { getStorage, ref, uploadBytes } = require("firebase/storage");
 const fs = require("fs");
 const path = require("path");
-const https = require("https");
-require("dotenv").config();
+const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
+const serviceAccount = require("./admin.json");
 
-const storage = new Storage({
-    credentials: serviceAccount,
-    projectId: serviceAccount.project_id,
-    agent: new https.Agent({
-        keepAlive: true,
-        timeout: 120000,
-    }),
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
 });
 
-const bucketName = "praiseup-37c47.appspot.com";
-const localFilePath = path.join(__dirname, "../../assets/bundle.json");
-const firebaseStoragePath = "bundle.json";
-
-const uploadFileWithMetadata = async () => {
-    try {
-        const fileBuffer = fs.readFileSync(localFilePath);
-        const data = JSON.parse(fileBuffer.toString());
-        const version = data.version || "1.0";
-
-        const bucket = storage.bucket(bucketName);
-        const file = bucket.file(firebaseStoragePath);
-
-        const metadata = {
-            metadata: {
-                customMetadata: {
-                    version: version,
-                    author: "Alex Bleotu",
-                    created_at: new Date().toISOString(),
-                },
-            },
-        };
-
-        await file.save(fileBuffer, { resumable: false });
-        await file.setMetadata(metadata);
-
-        console.log("File uploaded and metadata set successfully.");
-    } catch (err) {
-        console.error(
-            "Error uploading the file or setting metadata:",
-            err.message
-        );
-    }
+const firebaseConfig = {
+    apiKey: "AIzaSyARM9TMtALzUPscjZYtLlMcCNgrIz1roHw",
+    authDomain: "praiseup-37c47.firebaseapp.com",
+    projectId: "praiseup-37c47",
+    storageBucket: "praiseup-37c47.appspot.com",
+    messagingSenderId: "742969818984",
+    appId: "1:742969818984:web:414308e856683bce1560dd",
+    measurementId: "G-W2JXKPS0MR",
 };
 
-uploadFileWithMetadata();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+const uid = "fQqJ8YLnZaXVuBrzU3Tbp2NNtYx2";
+
+admin
+    .auth()
+    .createCustomToken(uid)
+    .then((customToken) => {
+        signInWithCustomToken(auth, customToken)
+            .then((userCredential) => {
+                const uploadFileWithMetadata = async (
+                    filePath,
+                    storagePath
+                ) => {
+                    try {
+                        const fileBuffer = fs.readFileSync(filePath);
+
+                        const fileRef = ref(storage, storagePath);
+
+                        const data = JSON.parse(fileBuffer.toString());
+
+                        const metadata = {
+                            customMetadata: {
+                                version: data.version,
+                                author: "Alex Bleotu",
+                                created_at: new Date().toISOString(),
+                            },
+                        };
+
+                        await uploadBytes(fileRef, fileBuffer, metadata);
+                        console.log(
+                            "Uploaded the bundle. Current version:",
+                            data.version
+                        );
+                    } catch (err) {
+                        console.error("Error uploading the file:", err);
+                    }
+                };
+
+                const localFilePath = path.join(
+                    __dirname,
+                    "../../assets/bundle.json"
+                );
+                const firebaseStoragePath = "bundle.json";
+
+                uploadFileWithMetadata(localFilePath, firebaseStoragePath);
+            })
+            .catch((error) => {
+                console.error("Error signing in with custom token:", error);
+            });
+    })
+    .catch((error) => {
+        console.error("Error creating custom token:", error);
+    });
